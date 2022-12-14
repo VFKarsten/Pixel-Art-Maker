c1ea51c50f436ae233fd5e3b51c27ec7d485fe5d
Karsten <120170504+VFKarsten@users.noreply.github.com>
2022-12-14 11:55:17 +0100

    Add files via upload
    2. try

--- designs.js
+++ designs.js
@@ -6,26 +6,26 @@ const HeightChoose = document.getElementById("inputHeight");
 const WidthChoose = document.getElementById("inputWidth");
 const sizePicker = document.getElementById("sizePicker");
 const pixelCanvas = document.getElementById("pixelCanvas");
- 
+
 //Prevent a default and use picker submit
-sizePicker.addEventListener ("submit", function(pixelArtMaker) {
-    pixelArtMaker.preventDefault();
-    while (pixelCanvas.hasChildNodes()) {
-        pixelCanvas.removeChild(pixelCanvas.lastChild);
-    }
-    makeGrid(); 
+sizePicker.addEventListener("submit", function(pixelArtMaker) {
+	pixelArtMaker.preventDefault();
+	while (pixelCanvas.hasChildNodes()) {
+		pixelCanvas.removeChild(pixelCanvas.lastChild);
+	}
+	makeGrid();
 });
 
 // When size is submitted by the user, call makeGrid()
 function makeGrid() {
-    for (var a = 0; a <= HeightChoose.value; a++) {
-        let line = pixelCanvas.insertRow();
-        for (var b = 0; b <= WidthChoose.value; b++) {  
-            let box = line.insertCell();
-            box.addEventListener("click", function() {
-                box.style.backgroundColor = colorButtom.value;
-                colorButtom.addEventListener("click", function(){});        
-        })
-        }
-    }                   
+	for (var a = 0; a < HeightChoose.value; a++) {
+		let line = pixelCanvas.insertRow();
+		for (var b = 0; b < WidthChoose.value; b++) {
+			let box = line.insertCell();
+			box.addEventListener("click", function() {
+				box.style.backgroundColor = colorButtom.value;
+				colorButtom.addEventListener("click", function() {});
+			});
+		}
+	}
 }
